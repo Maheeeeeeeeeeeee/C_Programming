ab6eb4e3cc107c8d43a27968830551b4816401bf
Maheeeeeeeeeeeee <maheshboke2003@gmail.com>
2025-11-24 12:23:32 +0530

    Update program1_4.c

--- Assignments/Assignment_1/program1_4.c
+++ Assignments/Assignment_1/program1_4.c
@@ -1,6 +1,17 @@
 #include<stdio.h>
 #include<stdbool.h>
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//      Function Name:  Check
+//      Description:    It is used to check whether the given number is divisible by 5 or not
+//      Input:          int
+//      Output:         bool
+//      Author:         Mahesh Dhanyakumar Boke
+//      Date:           24/1/2025
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 bool Check(int iNo)
 {
     if((iNo % 5) == 0)
