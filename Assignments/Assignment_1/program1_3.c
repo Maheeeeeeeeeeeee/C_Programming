bfab45d3c1f710c33b544a154035e9c833da524a
Maheeeeeeeeeeeee <maheshboke2003@gmail.com>
2025-11-24 12:20:07 +0530

    Update program1_3.c

--- Assignments/Assignment_1/program1_3.c
+++ Assignments/Assignment_1/program1_3.c
@@ -1,6 +1,18 @@
 #include<stdio.h>
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//      Function Name:  Display
+//      Description:    It is used to display numbers from 5 to 1 in decreasing order
+//      Input:          None
+//      Output:         int (but logically None, as it does not return any value)
+//      Author:         Mahesh Dhanyakumar Boke
+//      Date:           24/1/2025
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 int Display()
+void Display()
 {
     int i = 0;
     i =  5;
