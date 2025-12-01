8866ebd25889d23ba2322a5dcb10da3c4bd0c0af
Maheeeeeeeeeeeee <maheshboke2003@gmail.com>
2025-12-02 01:41:41 +0530

    Add files via upload

---
+++ Assignments/Assignment_28/program28_1.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//      Function Name:  Pattern
+//      Description:    Displays a pattern of letters starting from 'A' in each row for iRow rows and iCol columns
+//      Input:          int, int
+//      Output:         void
+//      Author:         Mahesh Dhanyakumar Boke
+//      Date:           01-12-2025
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void Pattern(int iRow, int iCol)
+{
+    int iP1 = 0, iP2 = 0;
+    char ch = 'A';
+
+    for(iP1 = 1; iP1 <= iRow; iP1++)
+    {
+        ch = 'A';
+        for(iP2 = 1; iP2 <= iCol; iP2++)
+        {
+            printf("%c  ", ch);
+            ch++;            
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows and columns");
+    scanf("%d %d",&iValue1, &iValue2);
+    
+    Pattern(iValue1, iValue2);
+    
+    return 0;
+}
---
+++ Assignments/Assignment_28/program28_4.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//      Function Name:  Pattern
+//      Description:    Displays a pattern where each row contains the same number, starting from iRow down to 1, repeated iCol times
+//      Input:          int, int
+//      Output:         void
+//      Author:         Mahesh Dhanyakumar Boke
+//      Date:           01-12-2025
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void Pattern(int iRow, int iCol)
+{
+    int iP1 = 0, iP2 = 0;
+
+    for(iP1 = iRow; iP1 > 0; iP1--)
+    {
+        for(iP2 = 1; iP2 <= iCol; iP2++)
+        {
+            printf("%d  ", iP1);            
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows and columns");
+    scanf("%d %d",&iValue1, &iValue2);
+    
+    Pattern(iValue1, iValue2);
+    
+    return 0;
+}
