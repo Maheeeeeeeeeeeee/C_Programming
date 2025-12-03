3ee9ba6a1b2d9973a51c1e0fad271539319b8099
Maheeeeeeeeeeeee <maheshboke2003@gmail.com>
2025-12-03 19:39:17 +0530

    Add files via upload

---
+++ Assignments/Assignment_32/program32_1.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//      Function Name:  Pattern
+//      Description:    Displays a pattern of * and # based on row and column positions
+//      Input:          int, int
+//      Output:         void
+//      Author:         Mahesh Dhanyakumar Boke
+//      Date:           03/12/2025
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1;j <= iCol; j++)
+        {
+            if(j == iCol - i + 1)
+            {
+                printf("#\t");
+            }
+            else
+            {
+                printf("*\t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &iValue1, &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
---
+++ Assignments/Assignment_32/program32_5.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//      Function Name:  Pattern
+//      Description:    Displays a numeric pattern forming borders and a diagonal from top-left to bottom-right
+//      Input:          int, int
+//      Output:         void
+//      Author:         Mahesh Dhanyakumar Boke
+//      Date:           03/12/2025
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+    
+    iRow++; 
+    iCol++;
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1;j <= iCol; j++)
+        {
+            if(i == j)
+            {
+                printf("%d\t", j);
+            }
+            else if(i == 1 || j == 1 || i == iRow || j == iCol)
+            {
+                printf("%d\t", j);
+            }
+            else
+            {
+                printf("\t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &iValue1, &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
